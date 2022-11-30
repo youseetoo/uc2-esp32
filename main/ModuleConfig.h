cea1472360631a8d73d15a9fd1a3f6eb2810efc6
beniroquai <bene.d@gmx.de>
2022-11-30 13:00:40 +0100

    removing unused modules

--- main/ModuleConfig.h
+++ main/ModuleConfig.h
@@ -4,15 +4,15 @@ struct ModuleConfig
 {
     bool analogout = false;
     bool dac = false;
-    bool digitalout = true;
-    bool digitalin = true;
+    bool digitalout = false;
+    bool digitalin = false;
     bool laser = true;
     bool led = true;
     bool config = true;
     bool motor = true;
     bool pid = false;
     bool scanner = false;
-    bool analogin = true;
+    bool analogin = false;
     bool slm = false;
-    bool home = true;
+    bool home = false;
 };
